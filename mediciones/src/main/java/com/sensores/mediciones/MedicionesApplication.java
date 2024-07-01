dc00877e00ba20165ba26f237e7ea9a4a4d55f6d
Cristo <cermija@gmail.com>
2024-07-01 12:12:25 -0600

    Subida de plantilla para trabajar en API de mediciones

---
+++ mediciones/src/main/java/com/sensores/mediciones/MedicionesApplication.java
@@ -0,0 +1,13 @@
+package com.sensores.mediciones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicionesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicionesApplication.class, args);
+	}
+
+}
